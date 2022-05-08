382285bf00237c4ec58d4cd4dd962dd79f19b368
Spencer <Spaopaopao@gmail.com>
2022-05-08 18:04:09 -0400

    renaming files regardless. (Seq. + png)

---
+++ bulk_mint_nfts/utils/rename_files_sequentially.py
@@ -0,0 +1,15 @@
+import glob
+import os
+
+
+def rename_files_sequentially(path_to_images: str, data_type: str) -> None:
+    """Converts names to sequential png format."""
+    image_list = glob.glob(f'{path_to_images}/*.{data_type}')
+    for c, i in enumerate(image_list):
+        new_name = i.split('\\')[0] + '\\'+f'{c}.png'
+        print(new_name)
+        os.rename(i, new_name)
+
+
+if __name__ == '__main__':
+    rename_files_sequentially(path_to_images='../images', data_type="jpg")
